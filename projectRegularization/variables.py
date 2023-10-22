50e107cbb09a1ba2ad8db9107aeab561b1e2da06
alisurajaya <47766351+alisurajaya@users.noreply.github.com>
2023-10-22 22:29:41 +0700

    Add files via upload

---
+++ projectRegularization/variables.py
@@ -0,0 +1,12 @@
+# CONFIGURE THE PATHS HERE: 
+
+
+DEBUG_DIR = "./debug/"
+
+# INFERENCE 
+INF_RGB = "/data/private/BPN_AI/mvt-ekstraksi-bangunan/dataset/img_tiles/*.png"
+INF_SEG = "/data/private/BPN_AI/mvt-ekstraksi-bangunan/predictions/*.tif"
+INF_OUT = "/data/private/BPN_AI/mvt-ekstraksi-bangunan/regularizations/"
+
+MODEL_ENCODER = "/data/private/BPN_AI/mvt-ekstraksi-bangunan/projectRegularization/pretrained_weights/E140000_e1"
+MODEL_GENERATOR = "/data/private/BPN_AI/mvt-ekstraksi-bangunan/projectRegularization/pretrained_weights/E140000_net"
